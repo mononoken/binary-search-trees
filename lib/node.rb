1e2c1339221ed601baf526cb2d1e6d01f319f82e
mononoken <ken.maeshima@pm.me>
2022-06-21 15:18:10 -0700

    Remove comment from lib/node.rb

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,4 @@
-# Build a Node class. It should have an attribute for the data it stores as well
-# as its left and right children. As a bonus, try including the Comparable
-# module and compare nodes using their data attribute.
+# frozen_string_literal: true
 
 # Node for use in a binary search tree (BST)
 class Node
